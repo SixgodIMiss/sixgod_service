29c0cc8ffddb182fe499ced5a3cdfbec7599f132
sixgod2 <1018681345@qq.com>
2019-03-22 00:46:16 +0800

    feature: at redis

--- frame/db/cache/Redis.php
+++ frame/db/cache/Redis.php
@@ -9,7 +9,35 @@
 namespace frame\db\cache;
 
 
+use frame\core\My_Config;
+
 class My_Redis
 {
+    protected $client;
+    private static $_instance = [];
 
+    /**
+     * My_Redis constructor.
+     * @param $config
+     */
+    protected function __construct($config)
+    {
+        $this->client = new \Redis();
+        $this->client->pconnect($config['host'], $config['port']);
+        $this->client->auth($config['password']);
+
+    }
+
+    /**
+     * @param string $db 这个地方要注意 是以链接为key 还是以数据库名为key
+     * @throws \Exception
+     */
+    public static function getInstance($db = 'default')
+    {
+        if (! arr_get(self::$_instance, $db, null) instanceof self) {
+            $config = My_Config::get('db', 'redis', 0);
+
+            self::$_instance[$db] = new self($config);
+        }
+    }
 }
